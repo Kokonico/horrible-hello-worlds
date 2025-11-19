b95eb1cf524a012151fce14f3b3508f8341b09ea
Kokonico <96935064+Kokonico@users.noreply.github.com>
2025-11-18 23:37:12 -0800

    python

---
+++ hello_world.py
@@ -0,0 +1,170 @@
+with open(
+    chr(next(iter(reversed(range(ord(max(str(int()))))))))
+    + chr(
+        len(
+            str(
+                set(
+                    enumerate(range(len(str(set(range(len(str(IndentationError()))))))))
+                )
+            )
+        )
+    )
+    + chr(
+        next(
+            iter(
+                reversed(
+                    range(len(str(bytearray(len(str(bytearray(len(str(complex())))))))))
+                )
+            )
+        )
+    )
+    + chr(len(str(set(range(ord(min(str(type(zip())))))))))
+    + chr(next(iter(reversed(range(ord(max(str(int()))))))))
+    + chr(ord(max(str(type(bool())))))
+    + chr(ord(max(str(type(vars())))))
+    + chr(
+        len(
+            str(
+                set(
+                    enumerate(range(len(str(set(range(len(str(IndentationError()))))))))
+                )
+            )
+        )
+    )
+    + chr(ord(max(str(print()))))
+    + chr(ord(max(str(type(tuple())))))
+    + chr(ord(max(str(type(vars()))))),
+    chr(next(iter(reversed(range(ord(max(str(object())))))))),
+) as f:
+    f.write(
+        chr(
+            len(
+                str(
+                    set(
+                        enumerate(
+                            enumerate(enumerate(range(len(str(IndentationError())))))
+                        )
+                    )
+                )
+            )
+        )
+        + chr(
+            next(
+                iter(
+                    reversed(
+                        range(
+                            len(
+                                str(bytearray(len(str(bytearray(len(str(complex())))))))
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        + chr(
+            len(
+                str(
+                    set(
+                        enumerate(
+                            enumerate(
+                                enumerate(
+                                    range(sum(range(len(str(IndentationError())))))
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        + chr(
+            len(
+                str(
+                    set(
+                        enumerate(
+                            enumerate(
+                                enumerate(
+                                    range(sum(range(len(str(IndentationError())))))
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        + chr(ord(max(str(print()))))
+        + min(str(dir()))
+        + chr(
+            next(
+                iter(
+                    reversed(
+                        range(
+                            next(
+                                iter(
+                                    reversed(
+                                        range(
+                                            next(
+                                                iter(
+                                                    reversed(
+                                                        range(
+                                                            len(
+                                                                str(
+                                                                    set(
+                                                                        enumerate(
+                                                                            enumerate(
+                                                                                enumerate(
+                                                                                    range(
+                                                                                        len(
+                                                                                            str(
+                                                                                                bool()
+                                                                                            )
+                                                                                        )
+                                                                                    )
+                                                                                )
+                                                                            )
+                                                                        )
+                                                                    )
+                                                                )
+                                                            )
+                                                        )
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        + chr(ord(max(str(print()))))
+        + chr(next(iter(reversed(range(ord(max(str(type(bool())))))))))
+        + chr(
+            len(
+                str(
+                    set(
+                        enumerate(
+                            enumerate(
+                                enumerate(
+                                    range(sum(range(len(str(IndentationError())))))
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        + chr(
+            len(
+                str(
+                    set(
+                        enumerate(
+                            range(len(str(set(range(len(str(IndentationError())))))))
+                        )
+                    )
+                )
+            )
+        )
+        + chr(len(repr(EOFError())))
+    )
