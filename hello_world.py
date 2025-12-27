69160f6f16863e6d05de0a52ecdd4edd469396e3
Kokonico <kokonico@duck.com>
2025-12-27 14:54:52 -0800

    hell (windows support)

--- hello_world.py
+++ hello_world.py
@@ -1,40 +1,49 @@
-with open(
-    chr(next(iter(reversed(range(ord(max(str(int()))))))))
-    + chr(
-        len(
-            str(
-                set(
-                    enumerate(range(len(str(set(range(len(str(IndentationError()))))))))
+with getattr(__builtins__, str().join(
+        [chr(ord(max(str(exec(str(dir())))))), chr(max(range(max(range(ord(max(str(range(int()))))))))),
+         next(reversed(str(bool()))), chr(max(range(ord(max(oct(int()))))))]))(
+        chr(next(iter(reversed(range(ord(max(str(int()))))))))
+        + chr(
+            len(
+                str(
+                    set(
+                        enumerate(range(len(str(set(range(len(str(IndentationError()))))))))
+                    )
                 )
             )
         )
-    )
-    + chr(
-        next(
-            iter(
-                reversed(
-                    range(len(str(bytearray(len(str(bytearray(len(str(complex())))))))))
+        + chr(
+            next(
+                iter(
+                    reversed(
+                        range(len(str(bytearray(len(str(bytearray(len(str(complex())))))))))
+                    )
                 )
             )
         )
-    )
-    + chr(len(str(set(range(ord(min(str(type(zip())))))))))
-    + chr(next(iter(reversed(range(ord(max(str(int()))))))))
-    + chr(ord(max(str(type(bool())))))
-    + chr(ord(max(str(type(vars())))))
-    + chr(
-        len(
-            str(
-                set(
-                    enumerate(range(len(str(set(range(len(str(IndentationError()))))))))
+        + chr(len(str(set(range(ord(min(str(type(zip())))))))))
+        + chr(next(iter(reversed(range(ord(max(str(int()))))))))
+        + chr(ord(max(str(type(bool())))))
+        + chr(ord(max(str(type(vars())))))
+        + chr(
+            len(
+                str(
+                    set(
+                        enumerate(range(len(str(set(range(len(str(IndentationError()))))))))
+                    )
                 )
             )
         )
-    )
-    + chr(ord(max(str(exec(str(dir()))))))
-    + chr(ord(max(str(type(tuple())))))
-    + chr(ord(max(str(type(vars()))))),
-    chr(next(iter(reversed(range(ord(max(str(object())))))))),
+        + chr(ord(max(str(exec(str(dir()))))))
+        + chr(ord(max(str(type(tuple())))))
+        + chr(ord(max(str(type(vars()))))) if getattr(__import__(max(oct(int())) + max(str(range))),
+                                                      chr(max(range(ord(max(oct(int())))))) + chr(
+                                                          len(str(ascii)) + len(str(help))) + chr(
+                                                          len(vars(list)) + len(str(help)) + int(not ())) + next(
+                                                          reversed(str(not ())))) == chr(
+            ord(max(oct(int()))) + (not ())) + max(oct(int())) + max(str(range)) + chr(
+            sum(range(len(repr(str(set)))))) + max(str(hex)) else next(iter(str(copyright))) + max(
+            oct(int())).upper() + chr(sum(range(len(str(set))))),
+        chr(next(iter(reversed(range(ord(max(str(object()))))))))
 ) as f:
     f.write(
         chr(
@@ -92,6 +101,7 @@ with open(
             )
         )
         + chr(ord(max(str(exec(str(dir()))))))
+        + max(str(tuple(repr(str()))))
         + min(str(dir()))
         + chr(
             next(
@@ -166,5 +176,6 @@ with open(
                 )
             )
         )
+        + chr((not())+ord(min(str(bin))))
         + chr(len(repr(EOFError())))
     )
